6e8a267828d9b59768f1756106352d06a53b535b
David Furlong <dvfurlong@googlemail.com>
2017-04-30 10:35:07 +0200

    Update game.js

--- game.js
+++ game.js
@@ -58,36 +58,90 @@ function TicTacToe(){
     return true;
 
   }
+    
     this.winner = function(board) {
+      /* 
+        This isn't how switch statements work.
+        The structure is almost correct, but the cases match on the value of the argument provided to switch.
+        In this case using if is probably easier, because you are not doing cases of a primitive. Each case will likely be checking
+        different parts of this.board.
+        
+        Switch statements are useful with primitive case matching on equality (string, number, ...)
+        
+        Example:
+        
+        var color = "red";
+        
+        switch(color) {
+          case "blue":
+            // do something
+          case "orange:
+            // do something
+          case "red":
+            // do something
+          default:
+            // do something if it doesn't match any
+        }
+        
+        
+        its just a shorter way of writing
+        
+        var color = "red";
+        if(color === "blue")
+          // do something
+        else if(color === "orange")
+          // do something
+        else if(color === "red")
+          // do something
+        else
+          // do something if it doesn't match any
+          
+        But if you dont feel comfortable with switch then just use if statements, they're just as good (and more flexible, for instance
+        for what we needed to do here)
+              
+      */
+      // correct case
+     /* 
+      [ 0 , 1 , 2 ,
+        3 , 4 , 5 ,
+        6 , 7 , 8 ]
+     */
+      // correct case (although doesn't work because of the way you used switch/case)
       switch(this.board[2] == this.board[4] && this.board[2] == this.board[6]) {
           return this.winner = this.board[2];
       }
+      // correct case
         case:this.board[0] == this.board[1] && this.board[0] == this.board[2]) {
             return this.winner = this.board[0];
          break;   
         }
-
+        // wrong case, look at the board
         case:this.board[3] == this.board[4] && this.board[0] == this.board[5]) {
             return this.winner = this.board[3];
       break;
         }
+      // correct case
         case:this.board[6] == this.board[7] && this.board[6] == this.board[8]) {
             return this.winner = this.board[6];
             break;
         }
+      // wrong case. Must be comparing exactly 3 squares at any case
         case:this.board[0] == this.board[3] && this.board[6] == this.board[7]) {
             return this.winner = this.board[0];
      break; 
       
         }
+      // correct
         case:this.board[1] == this.board[4] && this.board[1] == this.board[7]) {
             return this.winner = this.board[1];
       break;
         }
+      // correct
         case:this.board[2] == this.board[5] && this.board[2] == this.board[8]) {
             return this.winner = this.board[2];
       break;
         }
+      // correct
         case:this.board[0] == this.board[4] && this.board[0] == this.board[8]) {
             return this.winner = this.board[0];
       break;
